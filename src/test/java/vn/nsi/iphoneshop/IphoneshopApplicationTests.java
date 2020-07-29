d70f5520fc6d3030a41a4b913fab2d5e09492dbc
quang <nguyenvanquangk11@gmail.com>
2020-07-29 15:24:52 +0700

    commit on 29/07/2020

---
+++ src/test/java/vn/nsi/iphoneshop/IphoneshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.nsi.iphoneshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IphoneshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
